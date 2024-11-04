604c0b28ccb7b552b636b7024d8947a154479f99
danieleBattistelli <bomber81@gmail.com>
2024-11-04 19:11:23 +0100

    feat: add mail generator

--- 01-Mail/script.js
+++ 01-Mail/script.js
@@ -4,43 +4,27 @@
 //Non è necessario provvedere alla validazione delle email.
 
 //Genero email da una lista di nomi
-// function generateEmails(names, domain) {
-//     let emails = [];
-//     for (let i = 0; i < names.length; i++) {
-//         let email = names[i].toLowerCase().replace(/\s+/g, '.') + '@' + domain;
-//         emails.push(email);
-//     }
-//     return emails;
-// }
+ function generateEmails(names, domain) {
+     let emails = [];
+     for (let i = 0; i < names.length; i++) {
+         let email = names[i].toLowerCase().replace(/\s+/g, '.') + '@' + domain;
+         emails.push(email);
+     }
+     return emails;
+ }
 
-// let names = ["Mario Rossi", "Luigi Bianchi", "Anna Verdi", "Pinco Pallino", "Gina Score"];
-// let domain = "gmail.com";
-// let emailList = generateEmails(names, domain);
-// console.log("Questa è un' email list generata")
-// console.log(emailList.toString());
-
-// //Input
-// emailList = ['esempio1@esempio.com', 'esempio2@esempio.com', 'esempio3@esempio.com'];
-// const emailToCheck = prompt("Inserisci la tua email")
-// let emailFound = false;
-
-// for (let i = 0; i < emailList.length; i++) { 
-//     if (emailList[i] === emailToCheck) { 
-//         emailFound = true; break; } 
-//     }
-//         if (emailFound) { 
-//             console.log('Email trovata nella lista.'); 
-//         } else { 
-//             console.log('Email non trovata nella lista.'); 
-//         }
-
+let names = ["Mario Rossi", "Luigi Bianchi", "Anna Verdi", "Pinco Pallino", "Gina Score"];
+let domain = "gmail.com";
+let emailList = generateEmails(names, domain);
+console.log("Questa è un' email list generata")
+console.log(emailList.toString());
 
 // Lista di email
-const listaEmail = [
-    'esempio1@esempio.com',
-    'esempio2@esempio.com',
-    'esempio3@esempio.com'
-  ];
+// const listaEmail = [
+//     'esempio1@esempio.com',
+//     'esempio2@esempio.com',
+//     'esempio3@esempio.com'
+//   ];
   
   // Singola email da controllare
   const emailDaControllare = prompt("Iserisci la tua email")
@@ -50,8 +34,8 @@ const listaEmail = [
   let emailTrovata = false;
   
   // Ciclo attraverso la lista
-  for (let i = 0; i < listaEmail.length; i++) {
-    if (listaEmail[i] === emailDaControllare) {
+  for (let i = 0; i < emailList.length; i++) {
+    if (emailList[i] === emailDaControllare) {
       emailTrovata = true;
       break;
     }
